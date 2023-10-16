fba28eebfb576005d91f5695a4d701acb2dda45b
newnyee <mebbun0615@gmail.com>
2023-10-16 17:51:31 +0900

    Style: 불필요한 import 삭제

--- src/main/java/com/preonboarding/entity/Employ.java
+++ src/main/java/com/preonboarding/entity/Employ.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 import java.util.List;
