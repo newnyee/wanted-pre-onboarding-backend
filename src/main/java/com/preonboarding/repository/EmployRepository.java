e483337d419a1f58a88a0307d3fccb32ff511a61
newnyee <mebbun0615@gmail.com>
2023-10-17 18:38:06 +0900

    Feat: EmployRepository 코드 추가
    회사아이디, 채용공고 아이디로 employ 객체 찾기 메소드 추가

--- src/main/java/com/preonboarding/repository/EmployRepository.java
+++ src/main/java/com/preonboarding/repository/EmployRepository.java
@@ -1,8 +1,13 @@
 package com.preonboarding.repository;
 
+import com.preonboarding.entity.Company;
 import com.preonboarding.entity.Employ;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface EmployRepository extends JpaRepository<Employ, Long> {
+
+    Optional<Employ> findByEmployIdAndCompany(Long id, Company company);
 }
