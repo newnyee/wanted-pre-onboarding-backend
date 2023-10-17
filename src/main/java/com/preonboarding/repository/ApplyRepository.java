7e4a88050411f93c654f0e112fb31a08db89d205
newnyee <mebbun0615@gmail.com>
2023-10-17 21:42:52 +0900

    Refactor: Repository 코드 추가
    사용자 채용 공고 지원 로직을 위한 ApplyRepository.findByUserAndEmploy 코드 추가

--- src/main/java/com/preonboarding/repository/ApplyRepository.java
+++ src/main/java/com/preonboarding/repository/ApplyRepository.java
@@ -1,7 +1,13 @@
 package com.preonboarding.repository;
 
 import com.preonboarding.entity.Apply;
+import com.preonboarding.entity.Employ;
+import com.preonboarding.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
+    List<Apply> findByUserAndEmploy(User user, Employ employ);
 }
