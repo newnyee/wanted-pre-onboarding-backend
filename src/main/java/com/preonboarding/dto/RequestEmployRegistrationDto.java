35cfaba4c9f48617fd9be8d633de1c3f21179212
newnyee <mebbun0615@gmail.com>
2023-10-17 16:20:14 +0900

    Refactor: dto validation
    RequestEmployRegistrationDto 유효성검사 위한 어노테이션 추가

--- src/main/java/com/preonboarding/dto/RequestEmployRegistrationDto.java
+++ src/main/java/com/preonboarding/dto/RequestEmployRegistrationDto.java
@@ -1,6 +1,9 @@
 package com.preonboarding.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.validator.constraints.Range;
 
 @ToString
 @Getter
@@ -9,13 +12,20 @@ import lombok.*;
 @AllArgsConstructor
 public class RequestEmployRegistrationDto {
 
+    @NotNull
+    @Range(min = 0)
     private Long companyId;
 
+    @NotBlank
     private String employPosition;
 
+    @NotNull
+    @Range(min = 0)
     private Long employMoneyGift;
 
+    @NotBlank
     private String employContent;
 
+    @NotBlank
     private String employSkill;
 }
