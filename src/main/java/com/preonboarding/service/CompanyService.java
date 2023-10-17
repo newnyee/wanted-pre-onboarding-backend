32c2a9f22489c75c51e7dda9cf46991b722d0f0f
newnyee <mebbun0615@gmail.com>
2023-10-17 21:38:31 +0900

    Feat: Service 추가 (선택 사항 구현)
    사용자 채용 공고 지원을 위한 ApplyService 구현

--- src/main/java/com/preonboarding/service/CompanyService.java
+++ src/main/java/com/preonboarding/service/CompanyService.java
@@ -1,7 +1,25 @@
 package com.preonboarding.service;
 
 import com.preonboarding.entity.Company;
+import com.preonboarding.exception.NotFoundCompanyException;
+import com.preonboarding.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface CompanyService {
-    Company findById(Long id);
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public Company findById(Long id) {
+        return companyRepository.findById(id).orElseThrow(
+                () -> new NotFoundCompanyException("회사 ID를 확인해주세요.", HttpStatus.BAD_REQUEST)
+        );
+    }
 }
