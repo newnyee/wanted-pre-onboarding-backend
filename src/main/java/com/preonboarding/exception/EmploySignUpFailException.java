2f6415514d02ba7954c028ff36d11880485ccaf4
newnyee <mebbun0615@gmail.com>
2023-10-17 16:07:29 +0900

    Feat: 채용공고 등록 service exception 추가

---
+++ src/main/java/com/preonboarding/exception/EmploySignUpFailException.java
@@ -0,0 +1,17 @@
+package com.preonboarding.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+
+public class EmploySignUpFailException extends CommonException {
+
+    public EmploySignUpFailException(HttpStatus httpStatus) {
+        super(httpStatus);
+    }
+
+    public EmploySignUpFailException(String message, HttpStatus httpStatus) {
+        super(message, httpStatus);
+    }
+}
---
+++ src/main/java/com/preonboarding/exception/NotFoundCompanyException.java
@@ -0,0 +1,16 @@
+package com.preonboarding.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundCompanyException extends CommonException {
+
+    public NotFoundCompanyException(HttpStatus httpStatus) {
+        super(httpStatus);
+    }
+
+    public NotFoundCompanyException(String message, HttpStatus httpStatus) {
+        super(message, httpStatus);
+    }
+}
