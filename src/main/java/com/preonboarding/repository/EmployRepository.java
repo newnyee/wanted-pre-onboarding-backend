cf9adb85557670a54faaba9c9c750d7e7255a65a
newnyee <mebbun0615@gmail.com>
2023-10-17 19:52:45 +0900

    Feat: 메소드 추가 (선택 사항 구현)
    EmployRepository.findByCompanyAndEmployIdNot 메소드 추가

--- src/main/java/com/preonboarding/repository/EmployRepository.java
+++ src/main/java/com/preonboarding/repository/EmployRepository.java
@@ -4,10 +4,13 @@ import com.preonboarding.entity.Company;
 import com.preonboarding.entity.Employ;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface EmployRepository extends JpaRepository<Employ, Long> {
 
     Optional<Employ> findByEmployIdAndCompany(Long id, Company company);
+
+    List<Employ> findByCompanyAndEmployIdNot(Company company, Long employId);
 }
