bcf8fc81c791ee09f35e8c777671bfcead560d0a
newnyee <mebbun0615@gmail.com>
2023-10-18 13:44:46 +0900

    Refactor: 메소드 추가
    @BeforeEach 메소드 이용하여 테이블 초기화 & 중복 코드 제거

--- src/test/java/com/preonboarding/repository/EmployRepositoryTest.java
+++ src/test/java/com/preonboarding/repository/EmployRepositoryTest.java
@@ -1,15 +1,17 @@
 package com.preonboarding.repository;
 
-import com.preonboarding.dto.RequestEmployUpdateDto;
 import com.preonboarding.entity.Company;
 import com.preonboarding.entity.Employ;
 import com.preonboarding.service.CompanyService;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,74 +25,80 @@ class EmployRepositoryTest {
     EmployRepository employRepository;
 
     @Autowired
+    CompanyRepository companyRepository;
+
+    @Autowired
     CompanyService companyService;
 
-    @Test
-    void save() {
+    private Company company;
 
-        Company findCompany = companyService.findById(1L);
+    private Employ employ;
 
-        Employ employ = Employ.builder()
-                .company(findCompany)
+    @BeforeEach
+    void init() {
+
+        // 테이블 초기화
+        employRepository.deleteAll();
+        companyRepository.deleteAll();
+
+        // 샘플 데이터 insert
+        // 회사 가입
+        Company saveCompany = Company.builder()
+                .companyName("원티드랩")
+                .companyCountry("한국")
+                .companyArea("서울")
+                .build();
+        this.company = companyRepository.save(saveCompany);
+
+        // 채용공고 등록
+        Employ saveEmploy = Employ.builder()
+                .company(company)
                 .employPosition("백엔드 주니어 개발자")
                 .employContent("원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..")
                 .employMoneyGift(1000000L)
                 .employSkill("Python")
                 .build();
+        this.employ = employRepository.save(saveEmploy);
+    }
 
-        Employ savedEmploy = employRepository.save(employ);
-        Employ findEmploy = employRepository.findById(employ.getEmployId()).get();
-
-        assertThat(findEmploy).isEqualTo(savedEmploy);
+    @Test
+    void save() {
+        assertThat(employRepository.findById(employ.getEmployId()).get()).isEqualTo(employ);
     }
 
     @Test
     void updateEmploy() {
 
-        Company findCompany = companyService.findById(1L);
-
-        Employ employ = Employ.builder()
-                .company(findCompany)
-                .employPosition("백엔드 주니어 개발자")
-                .employContent("원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..")
-                .employMoneyGift(1000000L)
-                .employSkill("Python")
-                .build();
-
-        Long saveEmployId = employRepository.save(employ).getEmployId();
-
         Employ updateEmploy = Employ.builder()
-                .company(findCompany)
-                .employId(saveEmployId)
+                .company(company)
+                .employId(employ.getEmployId())
                 .employPosition("백엔드 주니어 개발자")
                 .employContent("원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..")
-                .employMoneyGift(1500000L)
+                .employMoneyGift(1500000L) //수정
                 .employSkill("Python")
                 .build();
 
-        Employ save = employRepository.save(updateEmploy);
-
-        assertThat(save.getEmployMoneyGift()).isEqualTo(1500000L);
+        assertThat(employRepository.save(updateEmploy).getEmployMoneyGift()).isEqualTo(1500000L);
     }
 
     @Test
     void delete() {
 
-        Company findCompany = companyService.findById(1L);
+        employRepository.deleteById(employ.getEmployId());
+        assertThat(employRepository.findById(employ.getEmployId())).isEmpty();
+    }
 
-        Employ employ = Employ.builder()
-                .company(findCompany)
-                .employPosition("백엔드 주니어 개발자")
-                .employContent("원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..")
-                .employMoneyGift(1000000L)
-                .employSkill("Python")
-                .build();
-        Employ savedEmploy = employRepository.save(employ);
-        Long employId = savedEmploy.getEmployId();
+    @Test
+    void findByEmployIdAndCompany() {
 
-        employRepository.deleteById(employId);
-        Optional<Employ> findemploy = employRepository.findById(employId);
+        Optional<Employ> getEmploy = employRepository.findByEmployIdAndCompany(employ.getEmployId(), company);
+        Assertions.assertThat(getEmploy).isNotEmpty();
+    }
 
-        assertThat(findemploy).isEmpty();
+    @Test
+    void findByCompanyAndEmployIdNot() {
+
+        List<Employ> getEmploy = employRepository.findByCompanyAndEmployIdNot(company, employ.getEmployId());
+        Assertions.assertThat(getEmploy).isEmpty();
     }
 }
