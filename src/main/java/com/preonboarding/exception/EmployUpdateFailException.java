038cafb41ab085f12ea84c95d1d9c4f76ad2840a
newnyee <mebbun0615@gmail.com>
2023-10-18 13:35:33 +0900

    Feat: 커스텀 예외 추가

---
+++ src/main/java/com/preonboarding/exception/EmployUpdateFailException.java
@@ -0,0 +1,17 @@
+package com.preonboarding.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+
+public class EmployUpdateFailException extends CommonException {
+
+    public EmployUpdateFailException(HttpStatus httpStatus) {
+        super(httpStatus);
+    }
+
+    public EmployUpdateFailException(String message, HttpStatus httpStatus) {
+        super(message, httpStatus);
+    }
+}
